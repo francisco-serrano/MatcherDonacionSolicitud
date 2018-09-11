99af896fbaace8769441ab56beae34582b678435
Lucas Vidaguren <lucas.vidaguren@econ.unicen.edu.ar>
2018-09-11 17:36:45 -0300

    Lematizador levanta bien el lemmatization desde jar

--- src/main/java/aplicacion/model/detector/Lematizador.java
+++ src/main/java/aplicacion/model/detector/Lematizador.java
@@ -1,11 +1,11 @@
 package aplicacion.model.detector;
 
 import com.google.common.base.Splitter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,10 +15,12 @@ public class Lematizador {
 
     public Lematizador() {
         try {
-            new BufferedReader(new FileReader(new File("lemmatization-es.txt"))).lines()
+            InputStream fileStream = ResourceUtils.getURL("classpath:lemmatization-es.txt").openStream();
+
+            new BufferedReader(new InputStreamReader(fileStream)).lines()
                     .map(linea -> Splitter.on('\t').splitToList(linea))
                     .forEach(parLemaPalabra -> diccionario_lemas.put(parLemaPalabra.get(1), parLemaPalabra.get(0)));
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
